1a5ceac1edc0596c06c6ef3f90ec35bb625c0621
Manucharyand <manucharyand@gmail.com>
2023-11-30 22:57:47 -0700

    created add skill and delete button

--- models/skill.js
+++ models/skill.js
@@ -7,7 +7,9 @@ const skills = [
 
 module.exports = {
     getAll,
-    getOne
+    getOne,
+    create,
+    deleteOne
 }
 
 function getAll() {
@@ -17,4 +19,17 @@ function getAll() {
 function getOne(id) {
     id = parseInt(id)
     return skills.find(skill => skill.id === id)
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000000
+    skill.done = false;
+    skills.push(skill)
+}
+
+function deleteOne(id) {
+    id = parseInt(id)
+    const idx = skills.findIndex(skill => skill.id === id)
+    console.log(idx + "THIS IS THE DELETE INDEX")
+    skills.splice(idx, 1)
 }
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require("method-override")
 
 var indexRouter = require('./routes/index');
 var skillsRouter = require('./routes/skills');
@@ -13,12 +14,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Middleware
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method"))
 
+//Routes
 app.use('/', indexRouter);
 app.use('/skills', skillsRouter);
 
